271962df7c8a027454d8993a044e7cdae042dba4
zhangliang26 <zhangliang26@meituan.com>
2019-04-30 11:20:42 +0800

    remove unused import

--- src/main/java/com/example/demo/stream/StreamTest.java
+++ src/main/java/com/example/demo/stream/StreamTest.java
@@ -4,7 +4,6 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
